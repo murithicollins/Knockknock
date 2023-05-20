0d2c3e146a0eb717292aef32fe8a09c0cbf3729d
murithicollins <105361953+murithicollins@users.noreply.github.com>
2023-05-20 17:54:54 +0300

    the team

--- src/routes/Team/+page.svelte
+++ src/routes/Team/+page.svelte
@@ -4,7 +4,7 @@
   <div class="">
     <img src="img/knock team.png" class="rounded-3xl " alt="" />
   </div>
-  <div class="w-1/2 my-10">
+  <div class="w-1/2 my-32">
     <h1 class="mb-4 text-xl font-bold leading-none tracking-tight text-[#EF6C00]">About Us</h1>
     <p class="">
       Knock knock was started in December 2021 with the goal of building a gadget that could alert a Deaf person when a knock happened on their door. However, with research and study into the target population, we realized that the problem dug deeper than just the inability of Deaf persons to know when someone is at their door.
@@ -15,27 +15,101 @@
 </div>
 
 
-<div class="">
-
-</div>
-<!-- <div class="container px-6 mx-auto">
+<div class="container my-24 px-6 mx-auto">
+  <!-- Section: Design Block -->
   <section class="mb-32 text-gray-800 text-center">
-    <h2 class="text-3xl font-bold mb-32">Meet the <u class="text-blue-600">team</u></h2>
-    <div class="grid gap-x-6 lg:gap-x-12 md:grid-cols-3">
-      <div class="mb-24 md:mb-0">
-         <div class="flex justify-center" style="margin-top: -75px">
-              <img src="https://mdbootstrap.com/img/new/avatars/6.jpg" class=" mx-auto shadow-lg" alt=""
-                style="width:" />
-         </div>
-        <div class="rounded-lg shadow-lg  w-56 absolute top-96 mx-8 block bg-white">
-          <div class="flex justify-center">
-                <div class="p-6">
-            <h5 class="text-lg font-bold mb-4">Marta Smith</h5>
-            <p class="mb-6">Frontend Developer</p>
-          </div> 
+    <h2 class="text-3xl font-bold mb-32">Team</h2>
+    <div class="grid gap-x-6  md:grid-cols-4">
+      <div class="flex  relative mb-24 md:mb-0">
+        <div class=" flex justify-center">
+          <div class="flex justify-center" style="">
+            <img src="img/Esther.jpg" class="mx-auto shadow-lg" alt=""
+              style="" />
+          </div>
+        </div>
+        <div class="flex justify-center">
+          <div class="shadow-lg absolute top-3/4 mt-20 inset-x-14 w-60 h-24 block bg-white">
+            <div class="w-full h-2 bg-[#00394F]"></div>
+            <div class="flex justify-center">
+              <div class="">
+                <h5 class="text-lg font-bold mt-2">Esther Mueni</h5>
+                <p class="">Team Lead</p>
+              </div>
+            </div>
+            <div>
+              <button class=" bg-[#EF6C00] text-white p-2 w-full rounded-md">Linked In</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex gap-10 relative mb-24 md:mb-0">
+        <div class=" flex justify-center">
+          <div class="flex justify-center" style="">
+            <img src="img/Brian.jpg" class="mx-auto shadow-lg" alt=""
+              style="" />
+          </div>
+        </div>
+        <div class="flex justify-center">
+          <div class="rounded-lg shadow-lg absolute top-3/4 mt-20 inset-x-10 w-60 h-24 block bg-white">
+            <div class="w-full h-2 bg-[#00394F]"></div>
+            <div class="flex justify-center">
+              <div class="">
+                <h5 class="text-lg font-bold mt-2">Brian Olasya</h5>
+                <p class="">Technical Lead</p>
+              </div>
+            </div>
+            <div>
+              <button class=" bg-[#EF6C00] text-white p-2 w-full rounded-md">Linked In</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex gap-10 relative mb-24 md:mb-0">
+        <div class=" flex justify-center">
+          <div class="flex justify-center" style="">
+            <img src="img/joe.jpg" class="mx-auto shadow-lg" alt=""
+              style="" />
+          </div>
+        </div>
+        <div class="flex justify-center">
+          <div class="rounded-lg shadow-lg absolute top-3/4 mt-20 inset-x-10 w-60 h-24 block bg-white">
+            <div class="w-full h-2 bg-[#00394F]"></div>
+            <div class="flex justify-center">
+              <div class="">
+                <h5 class="text-lg font-bold mt-2">Joseph Mwanik</h5>
+                <p class="">Operations Lead</p>
+              </div>
+            </div>
+            <div>
+              <button class=" bg-[#EF6C00] text-white p-2 w-full rounded-md">Linked In</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex gap-10 relative mb-24 md:mb-0">
+        <div class=" flex justify-center">
+          <div class="flex justify-center w-full" style="">
+            <img src="img/Dennis.png" class="mx-auto  shadow-lg" alt=""
+              style="" />
+          </div>
+        </div>
+        <div class="flex justify-center">
+          <div class="rounded-lg shadow-lg absolute top-3/4 mt-20 inset-x-10 w-60 h-24 block bg-white">
+            <div class="w-full h-2 bg-[#00394F]"></div>
+            <div class="flex justify-center">
+              <div class="">
+                <h5 class="text-lg font-bold mt-2">Dennis Kariuki </h5>
+                <p class="">Software engineer</p>
+              </div>
+            </div>
+            <div>
+              <button class=" bg-[#EF6C00] text-white p-2 w-full rounded-md">Linked In</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+
   </section>
-</div> -->
+</div>
+
