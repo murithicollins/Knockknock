bd7cfa95096f739f7791b3318d846c7148c618a6
murithicollins <105361953+murithicollins@users.noreply.github.com>
2023-05-24 11:41:43 +0300

    i bell update

--- src/routes/bell/+page.svelte
+++ src/routes/bell/+page.svelte
@@ -9,7 +9,7 @@
             <!--flex div  -->
             <div>
                 <div class="flex justify-center">
-                    <h3>The "normal" bell system</h3>
+                    <h3 class="mb-4 text-xl font-bold leading-none tracking-tight text-[#EF6C00]   dark:text-white">The "normal" bell system</h3>
                 </div>
                 <div>
                     <img src="img/normbell.svg" alt="">
@@ -20,7 +20,7 @@
         <div class="flex flex-col md:flex-row  justify-center bg-[#D9D9D9]  gap-8 w-full p-4 md:p-10 my-4 md:my-10">
             <!-- the problem -->
             <div class="w-full md:w-1/2 p-4 md:p-10 my-4 md:my-10">
-                <h1 class="flex items-center justify-center">The problem</h1>
+                <h1 class="flex items-center justify-center mb-4 text-xl font-bold leading-none tracking-tight text-[#EF6C00]   dark:text-white">The problem</h1>
                 <p>The school bell system i impolrtant in creating  wel-organize structure for smooth running of activities in a school. It is used to announce critical times in the school, create routine, and to get attention 
                     in cases of emergencies. Most schools for the Deaf in Kenya use the ‘conventional’ 
                     sound based bell system which is ineffective because a majority of the students are Deaf. This implies the system is ineffective which could lead to - lack of respect for time, excuses for lateness, anda  decline in teaching quality.
@@ -28,7 +28,7 @@
             </div>
             <!-- about -->
             <div class="w-full md:w-1/2 p-4 md:p-10 my-4 md:my-10">
-                <h1 class="flex justify-center">About i-bell:</h1>
+                <h1 class="flex justify-center mb-4 text-xl font-bold leading-none tracking-tight text-[#EF6C00]   dark:text-white">About i-bell:</h1>
                 <p>
                     I-bell is a bell system that provides visual and sound alerts to capture attention of both hearing and Deaf students and staff within schools for the Deaf.
 
@@ -42,7 +42,7 @@
             <!--flex div  -->
             <div>
                 <div class="flex justify-center">
-                    <h3>The i-bell system</h3>
+                    <h3 class="mb-4 text-xl font-bold leading-none tracking-tight text-[#EF6C00]   dark:text-white">The i-bell system</h3>
                 </div>
                 <div>
                     <img src="img/ibell.svg" alt="">
@@ -54,13 +54,13 @@
             <div>
                 <!-- title -->
                 <div class="flex items-center justify-center" >
-                    <h1>Our Strategy</h1>
+                    <h1 class="mb-4 text-xl font-bold leading-none tracking-tight text-[#EF6C00]   dark:text-white">Our Strategy</h1>
                 </div>
                 <!-- context -->
                 <div class="p-4">
                 <p>Our intention is to implement i-bell to all schools for the Deaf and integrated schools in Kenya as we move to those across Africa. How we can partner :  </p> 
                 </div>
-                <div class="flex flex-col justify-center my-2 md:my-4 md:my-8 w-full md:w-5/6 p-4">
+                <div class="flex flex-col justify-center my-2  md:my-8 w-full md:w-5/6 p-4">
                     <p>
                         ✓ Implementation partners: 
                         <span>  
