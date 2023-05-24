cb88bdfd72335db44e3f068ef55baf9b0ca46c0d
murithicollins <105361953+murithicollins@users.noreply.github.com>
2023-05-24 14:59:16 +0300

    all commits without the baby monitor

--- src/routes/Team/+page.svelte
+++ src/routes/Team/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 </script>
-<div class="flex justify-center gap-10 shadow-2xl w-full bg-[#D9D9D9] p-10">
+<div class="lg:flex md:justify-center md:gap-10 shadow-2xl md:w-full bg-[#D9D9D9] p-10">
   <div class="">
     <img src="img/knock team.png" class="rounded-3xl " alt="" />
   </div>
-  <div class="w-1/2 my-32">
-    <h1 class="mb-4 text-xl font-bold leading-none tracking-tight text-[#EF6C00]">About Us</h1>
-    <p class="">
+  <div class="my-32 lg:w-full w-4/5">
+    <h1 class="mb-4 text-3xl font-bold leading-none tracking-tight text-[#EF6C00]">About Us</h1>
+    <p class="text-xl">
       Knock knock was started in December 2021 with the goal of building a gadget that could alert a Deaf person when a knock happened on their door. However, with research and study into the target population, we realized that the problem dug deeper than just the inability of Deaf persons to know when someone is at their door.
         Stories from the Deaf community revealed how crippled this population
         was by their inability to perceive sounds; which pushed us to think deeper, not just in terms of a product but what impact we, as experienced techies, could make on this community.
@@ -17,8 +17,8 @@
 
 <div class="container my-24 px-6 mx-auto">
   <!-- Section: Design Block -->
-  <section class="mb-32 text-gray-800 text-center">
-    <h2 class="text-3xl font-bold mb-32">Team</h2>
+  <section class="mb-32  text-center">
+    <h2 class="mb-10 text-3xl font-bold leading-none tracking-tight text-[#EF6C00] ">Team</h2>
     <div class="grid gap-x-6  md:grid-cols-4">
       <div class="flex  relative mb-24 md:mb-0">
         <div class=" flex justify-center">
