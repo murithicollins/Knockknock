82ecae372c19c7fa51b688f284d1b8ca21b80a77
your_github_username <anthonymathenge191@gmail.com>
2023-05-26 13:51:29 +0300

    anto's updates

--- src/components/herosection.svelte
+++ src/components/herosection.svelte
@@ -2,23 +2,59 @@
     
 </script>
 
-<div class="flex flex-col-reverse py-4 md:py-8 px-4 flex-col md:flex-row">
-    <div class="flex justify-center mt-20 inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0 ">
-      <img src="img/knock team.png" class="image my-4 md:my-8" alt="" />
-    </div>
-    <div class=" flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
-      <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-            <h2 class="mb-5  text-xl font-bold font-primary  text-[#EF6C00]">KNOCK KNOCK GADGET<br class="hidden md:block" /></h2>
-            <p class="pr-5 mb-5 text-[#00394F] md:text-24 font-secondary">
-                listens to sounds in the environment and conveys them in a way perceivable by persons with hearing loss..
+<!-- heros section -->
+<main class="flex justify-center items-center mx-10 my-5 ">
+  <!-- main div -->
+  <div class="">
+    <!-- secondary div -->
+    <div class="flex flex-col md:flex-row gap-2 md:gap-2 ">
+      <!-- context -->
+      <div class="grow w-full md:w-1/2 flex items-center justify-center ">
+        <div class="space-y-8">
+          <div>
+            <h1 class="text-4xl font-bold font-primary  text-[#EF6C00]">
+              KNOCK KNOCK GADGET
+            </h1>
+          </div>
+          <div>
+            <p class="text-[#00394F] md:text-24 font-secondary pr-5 mb-5">
+              listens to sounds in the environment and conveys them in a way perceivable by persons with hearing loss..
             </p>
-            <div class="flex items-center ">
-            <a
-                href="mailto:info@knockknock.co.ke"class="inline-flex items-center justify-center h-12 px-6 mr-6 font-tertiary  font-bold  text-white transition duration-200 rounded-full shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none bg-[#00394F]">Partner With Us</a>
-            </div>
+          </div>
+          <div>
+            <button>
+              <a
+                  href="mailto:info@knockknock.co.ke" class="font-tertiary font-bold  text-white  bg-[#00394F] p-4 rounded-full ">Partner With Us
+              </a>
+              
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- img -->
+      <div class="w-full md:w-1/2">
+        <img src="img/knock team.png" class="image " alt="" />
       </div>
     </div>
-</div>
+
+  </div>
+</main>
+
+ <!-- <div class="flex flex-col ">
+    <div class="relative"> 
+      <div class="">
+            <h2 class=" "><br class="hidden md:block" /></h2>
+            <p class=" ">
+                
+            </p>
+            <div class="flex items-center ">
+            
+      </div>
+    </div>
+    <div class="flex justify-center mt-20 inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:absolute xl:px-0 ">
+      
+    </div>
+    </div> --> 
 <style>
 .image{
   clip-path: polygon(10% 0, 100% 0, 100% 100%, 0% 100%);
