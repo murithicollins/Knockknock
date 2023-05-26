d0017dce4696bb6698921d977d05500ff5eece07
murithicollins <105361953+murithicollins@users.noreply.github.com>
2023-05-26 16:00:43 +0300

    one commit

--- src/components/herosection.svelte
+++ src/components/herosection.svelte
@@ -2,19 +2,19 @@
     
 </script>
 
-<div class="flex flex-col-reverse py-4 md:py-8 px-4 flex-col md:flex-row">
+<div class="flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 my-10">
     <div class="flex justify-center mt-20 inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0 ">
-      <img src="img/knock team.png" class="image my-4 md:my-8" alt="" />
+      <img src="img/team.jpg" class="image h-1/2 my-10" alt="" />
     </div>
     <div class=" flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
       <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-            <h2 class="mb-5  text-xl font-bold font-primary  text-[#EF6C00]">KNOCK KNOCK GADGET<br class="hidden md:block" /></h2>
-            <p class="pr-5 mb-5 text-[#00394F] md:text-24 font-secondary">
+            <h2 class="mb-5 font-sans text-3xl font-bold tracking-tight text-[#EF6C00] sm:text-4xl sm:leading-none">KNOCK KNOCK GADGET<br class="hidden md:block" /></h2>
+            <p class="pr-5 mb-5 text-[#00394F] md:text-lg">
                 listens to sounds in the environment and conveys them in a way perceivable by persons with hearing loss..
             </p>
             <div class="flex items-center ">
             <a
-                href="mailto:info@knockknock.co.ke"class="inline-flex items-center justify-center h-12 px-6 mr-6 font-tertiary  font-bold  text-white transition duration-200 rounded-full shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none bg-[#00394F]">Partner With Us</a>
+                href="mailto:info@knockknock.co.ke"class="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded-full shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none bg-cyan-950">Partner With Us</a>
             </div>
       </div>
     </div>
@@ -23,14 +23,4 @@
 .image{
   clip-path: polygon(10% 0, 100% 0, 100% 100%, 0% 100%);
 }
-.font-primary {
-      font-family: "Quicksand", sans-serif;
-    }
-.font-secondary {
-      font-family: sans-serif;
-    }
-    
-.font-tertiary {
-      font-family: "Museo Slab", serif;
-    }
 </style>
